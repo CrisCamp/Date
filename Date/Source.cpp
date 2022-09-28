59f75c879c3aacb95996afc7484b7acb517d3844
CrisCamp <cristophercc0204@gmail.com>
2022-09-28 05:08:14 -0500

    Actualizacion, nuevas validaciones y cambios en el codigo

--- Date/Source.cpp
+++ Date/Source.cpp
@@ -8,7 +8,7 @@ using namespace std;
 class Date
 {
 private:
-	unsigned int dia, mes, año, longitud;
+	int dia, mes, año, longitud;
 	string  year, mounth, day, fechatem;
 public:
 	Date(string fecha)
@@ -49,13 +49,95 @@ public:
 		cout << mes << "\n";
 		cout << dia << "\n";*/
 	}
-	operator const char*()
+	operator const char* ()
 	{
 		ostringstream a;
-		a << year << " / " << mounth << " / " << day;
+		a << limitarAño() << " / " << limitarMes() << " / " << limitarDia();
 		fechatem = a.str();
 		//cout << "\n" << a.str() << "\n";
 		return fechatem.c_str();
+	}
+	int limitarAño()
+	{
+		if (año < 1500)
+		{
+			año = 1500;
+		}
+		else if (año > 3500)
+		{
+			año = 3500;
+		}
+		return año;
+	}
+	bool biciesto()
+	{
+		if (limitarAño() % 4 == 0 and limitarAño() % 100 != 0 or limitarAño() % 400 == 0)
+		{
+			return true;
+		}//Condición de año bisiesto
+		else
+		{
+			return false;
+		}
+	}
+	int limitarMes()
+	{
+		if (mes > 12)
+		{
+			mes = 12;
+		}
+		else if (mes <= 0) //el operador "<" esta de mas porque la variable mes esta declara como int, por lo que se pueden presentar numeros negativos
+		{
+			mes = 1;
+		}
+		return mes;
+	}
+	int limitarDia()
+	{
+		if (limitarMes() == 1 || mes == 3 || mes == 5 || mes == 7 || mes == 8 || mes == 10 || limitarMes() == 12)
+		{
+			if (dia > 31)
+			{
+				dia = 31;
+			}
+			else if (dia <= 0)
+			{
+				dia = 1;
+			}
+		}
+		else if (mes == 4 || mes == 6 || mes == 9 || mes == 11)
+		{
+			if (dia > 30)
+			{
+				dia = 30;
+			}
+			else if (dia <= 0)
+			{
+				dia = 1;
+			}
+		}
+		else if (mes == 2)
+		{
+			if (biciesto() == true)
+			{
+				if (dia > 29)
+				{
+					dia = 29;
+				}
+			}
+			else
+			{
+				if (dia > 28)
+				{
+					dia = 28;
+				}
+			}
+			if (dia <= 0)
+			{
+				dia = 1;
+			}
+		}		
+		return dia;
 	}
 	void validarFecha()
 	{
@@ -74,7 +156,7 @@ public:
 			int cm = 0;
 			if (nm < 10)
 			{
-				cm++;
+				cm = 1;
 			}
 			while (nm > 0)
 			{
@@ -84,7 +166,7 @@ public:
 			int cd = 0;
 			if (nd < 10)
 			{
-				cd++;
+				cd = 1;
 			}
 			while (nd > 0)
 			{
@@ -101,81 +183,110 @@ public:
 				{
 					if (dayEnt == cd)
 					{
-						if (año % 4 == 0 and año % 100 != 0 or año % 400 == 0) //Condición de año bisiesto
+						if (mes >= 1 && mes <= 12)
 						{
-							cout << "\nEl año introducido es Bisiesto\n" << endl;
-							if (mes >= 1 && mes <= 12)
+							if ((mes == 1 || mes == 3 || mes == 5 || mes == 7 || mes == 8 || mes == 10 || mes == 12) && (dia >= 1 && dia <= 31))
 							{
-								if ((mes == 1 || mes == 3 || mes == 5 || mes == 7 || mes == 8 || mes == 10 || mes == 12) && (dia >= 1 && dia <= 31))
+								if (biciesto() == true)
 								{
-									cout << "Fecha valida\n" << endl;
-								}							
-								else if  ((mes == 4 || mes == 6 || mes == 9 || mes == 11) && (dia >= 1 && dia <= 30))
-								{
-									cout << "Fecha valida\n" << endl;
-								}
-								else if ((mes == 2 && dia >= 1 && dia <= 29))
-								{
-									cout << "Fecha valida\n" << endl;										
-								}
-								else 
-								{
-									cout << "El dia no existe\n" << endl;
-								}								
-							}
-							else
-							{
-								cout << "El mes no existe\n" << endl;
-							}
-						}
-						else
-						{
-							cout << "\nEl año introducido no es bisiesto\n" << endl;
-							if (mes >= 1 && mes <= 12)
-							{
-								if ((mes == 1 || mes == 3 || mes == 5 || mes == 7 || mes == 8 || mes == 10 || mes == 12) && (dia >= 1 && dia <= 31))
-								{
-									cout << "Fecha valida\n" << endl;
-									cout << dia << endl;
-								}
-								else if ((mes == 4 || mes == 6 || mes == 9 || mes == 11) && (dia >= 1 && dia <= 30))
-								{
-									cout << "Fecha valida\n" << endl;
-								}
-								else if ((mes == 2 && dia >= 1 && dia <= 28))
-								{
-									cout << "Fecha valida\n" << endl;
+									cout << "\nEl año introducido es Bisiesto\n" << endl;
 								}
 								else
 								{
-									cout << "El dia o la fecha no existe\n" << endl;
+									cout << "\nEl año introducido no es Bisiesto\n" << endl;
+								}
+							}							
+							else if  ((mes == 4 || mes == 6 || mes == 9 || mes == 11) && (dia >= 1 && dia <= 30))
+							{
+								if (biciesto() == true)
+								{
+									cout << "\nEl año introducido es Bisiesto\n" << endl;
+								}
+								else
+								{
+									cout << "\nEl año introducido no es Bisiesto\n" << endl;
 								}
 							}
-							else
-							{
-								cout << "El mes no existe\n" << endl;
+							else if ((biciesto() == true) && mes == 2 && dia >= 1 && dia <= 29)
+							{									
+								cout << "\nEl año introducido es Bisiesto\n" << endl;
 							}
+							else if ((biciesto() == false) && mes == 2 && dia >= 1 && dia <= 28)
+							{
+								cout << "\nEl año introducido no es Bisiesto\n" << endl;
+							}
+							else 
+							{
+								if (biciesto() == false)
+								{
+									cout << "\nEl año introducido no es Bisiesto\n" << endl;
+								}
+								cout << "la fecha no existe por el dia establecido, por defecto y correspondiente al mes se dejara en: " << limitarDia() << endl;
+							}								
+						}
+						else
+						{
+							cout << "El mes no existe, por defecto se dejara en: \n"<< limitarMes() << endl;
 						}
 					}
 					else
 					{
-						cout << "\nDato invalido en dia\n\n";
+						cout << "\nDato invalido, por defecto se establecera como: \n";
+						//cout << "\nDato invalido en dia\n";
 					}
 				}
 				else
 				{
-					cout << "\nDato invalido en mes\n\n";
+					cout << "\nDato invalido, por defecto se establecera como: \n";
+					//cout << "\nDato invalido en mes\n";
 				}
 			}
 			else
 			{
-				cout << "\nDato invalido en año\n\n";
+				cout << "\nDato invalido, por defecto se establecera como: \n";
+				//cout << "\nDato invalido en año\n";
 			}
+			cout << operator const char* () << "\n";
 		}
 		else
 		{
-			cout << "\nRecuerde respetar el formato indicado, asegurese de llenar y no 'sobrepasar' los espacios\n\n";
+			cout << "\nRecuerde respetar el formato indicado, asegurese de 'llenar' y no 'sobrepasar' los espacios\n\n";
 		}
+	}
+	void incrementarFecha ()
+	{
+		cout << "\nSumando el dato establecido mas un dia:\n";
+		int diaSum = limitarDia();
+		int mesSum = limitarMes();
+		int añoSum = limitarAño();
+
+		diaSum++;
+		if ((limitarMes() == 1 || mes == 3 || mes == 5 || mes == 7 || mes == 8 || mes == 10 || limitarMes() == 12) && limitarDia() == 31)
+		{
+			diaSum = 1;
+			mesSum++;
+			if (limitarMes() == 12)
+			{
+				mesSum = 1;
+				añoSum++;
+			}
+		}
+		else if ((mes == 4 || mes == 6 || mes == 9 || mes == 11) && limitarDia() == 30)
+		{
+			diaSum = 1;
+			mesSum++;
+		}
+		else if ((biciesto() == true && mes == 2) && limitarDia() == 29)
+		{
+			diaSum = 1;
+			mesSum++;
+		}
+		else if ((biciesto() == false && mes == 2) && limitarDia() == 28)
+		{
+			diaSum = 1;
+			mesSum++;
+		}
+		cout << añoSum << " / " << mesSum << " / " << diaSum <<"\n\n";
 	}
 };
 	class Inicio
@@ -196,7 +307,8 @@ public:
 				cin >> fecha;
 				Date* fe = new Date(fecha);
 				fe->validarFecha();
-				cout << "\n" << fe->operator const char* () << "\n\n";
+				//cout << "\n" << fe->operator const char* () << "\n\n";
+				fe->incrementarFecha();
 				cout << "Introduzca la opcion\n1. Seguir en el programa\n2. Salir del programa" << endl;
 				cin >> opcion;
 				system("cls");
